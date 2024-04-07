39785f5273f19f6f6dfcc141127d700a428f9b05
LocalSpook <chernyakin.victor.j@outlook.com>
2024-04-06 20:53:50 -0700

    Handle exceptions so they can't escape out of main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <exception>
 #include <print>
 
-int main() {
+int main() try {
 	std::println("Hello world!");
 	return 0;
+} catch (const std::exception& e) {
+	try {
+		std::println("Exception: {}", e.what());
+	} catch (...) {
+		return 1;
+	}
+	return 1;
+} catch (...) {
+	return 1;
 }
